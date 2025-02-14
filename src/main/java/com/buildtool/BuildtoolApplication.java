4e7c37b0c11d2dc18c2a8c9a8f95ee06baf50489
Rahulstar177 <rathodsachiit@gmaiil.com>
2025-02-14 18:52:29 +0530

    this is by rahul

---
+++ src/main/java/com/buildtool/BuildtoolApplication.java
@@ -0,0 +1,14 @@
+package com.buildtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildtoolApplication.class, args);
+	
+	}
+
+}
---
+++ src/main/java/com/buildtool/add/Sum.java
@@ -0,0 +1,12 @@
+package com.buildtool.add;
+
+public class Sum {
+	
+	
+	
+	public int sum(int a , int b)
+	{
+		return a+b;
+	}
+
+}
